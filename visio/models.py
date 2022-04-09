022c935a0403ebae6c22d7b7881430e52d7098ae
Alessandro Pellegrino <alessandro.pellegrino@prodigysgroup.it>
2022-04-09 09:53:02 +0200

    init

--- visio/models.py
+++ visio/models.py
@@ -1,3 +1,27 @@
+from turtle import mode
 from django.db import models
 
-# Create your models here.
+class Camera(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1024, null=True, blank=True, default='Visio standard camera')
+    desc = models.TextField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Videocamera"
+        verbose_name_plural = "Videocamere"
+        
+        
+class Entity(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=1024, null=True, blank=True, default="")
+    desc = models.TextField(null=True, blank=True)
+    site = models.CharField(max_length=2048, null=True, blank=True)
+    camera = models.ForeignKey(Camera, blank=True, null=True, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+
